939af88b41600bf8e8cdfa2ee2af54b4db84cd8e
emoldino-steve <steve.jung@emoldino.com>
2022-02-27 03:09:52 +0900

    Remove @EnableWebMvc

--- src/main/java/junguitar/framework/resource/dbtable/config/SwaggerConfig.java
+++ src/main/java/junguitar/framework/resource/dbtable/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package junguitar.framework.resource.dbtable.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -12,7 +11,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-@EnableWebMvc
 @Profile("!prod")
 public class SwaggerConfig {
 
